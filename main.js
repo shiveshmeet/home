21c1fed427ba070f87e46de7b1a2253d4d1c2975
shiveshmeet <73155716+shiveshmeet@users.noreply.github.com>
2020-12-08 09:50:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function join() {
+	window.open("/join/index.html" , "_self")
+}
+function download() {
+	window.open("/join/index.html" , "_self")
+}
+function signin() {
+	window.open("/signin/index.html" , "_self")
+}
+function join() {
+	window.open("/join/index.html" , "_self")
+}
+function signup() {
+	window.open("/signup/index.html" , "_self")
+}
